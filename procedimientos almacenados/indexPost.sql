e7deb63795cb6f210ba77aaa8ac614a55a8621e5
Jean Contreras <jeancon29@gmail.com>
2019-12-21 09:15:51 -0400

    Se creo nuevo funcion sql idVenta

--- procedimientos almacenados/indexPost.sql
+++ procedimientos almacenados/indexPost.sql
@@ -137,36 +137,7 @@ select JSON_ARRAYAGG(JSON_OBJECT(
     
     
     -- 				CREAR IDVENTA TEMPORAL 
-		select max(id) from sales into siguienteIdVenta;
-        if siguienteIdVenta is null then
-			set siguienteIdVenta = 0;
-		end if;
-		set siguienteIdVenta = siguienteIdVenta + 1;
-        
-        select idventatemporals.idBanca from idventatemporals where idventatemporals.idVenta = siguienteIdVenta into idBancaIdVentaTemporal;
-        if idBancaIdVentaTemporal is not null then
-			if idBanca = idBancaIdVentaTemporal then
-				 select idventatemporals.idVentaHash from idventatemporals where idventatemporals.idVenta = siguienteIdVenta into idVentaHash;
-            else 
-				select max(idventatemporals.idVenta) from idventatemporals into siguienteIdVenta;
-                if siguienteIdVenta is null then
-					set siguienteIdVenta = 0;
-				end if;
-				set siguienteIdVenta = siguienteIdVenta + 1;
-            end if;
-		end if;
-        
-        if idVentaHash is null then
-			set @idVentaHash = AES_ENCRYPT(siguienteIdVenta, 'Sistema de loteria jean y valentin');
-           -- AES_ENCRYPT retorna BLOB entonces lo convertimos a hex
-           set @idVentaHash = HEX(@idVentaHash);
-           set idVentaHash = @idVentaHash;
-           -- para desencriptar le quitamos el HEX con la funcion UNHEX  
-           -- select CAST(AES_DECRYPT(UNHEX(@j), 'hola') AS CHAR(50)) as desencriptado;
-			-- select CAST(AES_DECRYPT(@j, 'Sistema de loteria jean y valentin') AS CHAR(50)) as desencriptado;
-            -- select 'dentro insert idVentaHash';
-			 insert into idventatemporals(idventatemporals.idBanca, idventatemporals.idVenta, idventatemporals.idVentaHash) values(idBanca, siguienteIdVenta, idVentaHash);
-		end if;
+		select idVenta(idBanca) into idVentaHash;
           
     select ventas, idBanca, total_ventas, total_jugadas, caracteristicasGenerales, pidUsuario, bancas, loterias, idVentaHash;
    
