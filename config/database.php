cde14325eb9daccbb43e7811b38fb5ad5c9e9108
Jean Contreras <jeancon29@gmail.com>
2020-06-30 22:32:16 -0400

    Se agrego el servidor valentin al archivo config/database.php

--- config/database.php
+++ config/database.php
@@ -100,6 +100,21 @@ return [
             'strict' => true,
             'engine' => null,
         ],
+        'valentin' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => "valentin",
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+        ],
 
         
 
