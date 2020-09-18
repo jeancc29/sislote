3139791ed5c14881ebd5f2bed69821e128e2fd1e
Jean Contreras <jeancon29@gmail.com>
2020-09-18 12:24:31 -0400

    Prueba error TicketToHtmlClass

--- app/Classes/TicketToHtmlClass.php
+++ app/Classes/TicketToHtmlClass.php
@@ -425,7 +425,7 @@ class TicketToHtmlClass{
         $hora = $fecha->format('g:i A');
         } catch (\Throwable $th) {
             // throw $this->venta->created_at;
-            abort(402, $this->venta->created_at["date"]);
+            abort(402, json_encode($this->venta->created_at));
         }
 
         $fechaCompleta = str_replace('-', '/', $fecha->toDateString()) . " " . $hora;
